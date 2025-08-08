5e81a6ce3a803e9a675f1eeb3469c101a7b25ee7
Zeta_Null <z0zetanull@gmail.com>
2025-08-07 21:09:10 -0400

    Music Bugfix from the Pokecrystal docs

--- constants/music_constants.asm
+++ constants/music_constants.asm
@@ -130,13 +130,12 @@
 	const MUSIC_FOURISLAND					 ; 7d (We're coming up on $80, there's a fix in the pokecrystal design flaws section but that and above may be buggy)
 DEF NUM_MUSIC_SONGS EQU const_value
 
-; GetMapMusic picks music for this value (see home/map.asm)
-DEF MUSIC_MAHOGANY_MART EQU $64
+; GetMapMusic picks music for these values (see home/map.asm)
+DEF MUSIC_MAHOGANY_MART EQU $fc
+DEF MUSIC_RADIO_TOWER   EQU $fd
 
 ; ExitPokegearRadio_HandleMusic uses these values
 DEF RESTART_MAP_MUSIC EQU $fe
 DEF ENTER_MAP_MUSIC   EQU $ff
 
-; GetMapMusic picks music for this bit flag
-DEF RADIO_TOWER_MUSIC_F EQU 7
-DEF RADIO_TOWER_MUSIC EQU 1 << RADIO_TOWER_MUSIC_F
+assert NUM_MUSIC_SONGS <= MUSIC_MAHOGANY_MART, "song IDs overlap special values"
